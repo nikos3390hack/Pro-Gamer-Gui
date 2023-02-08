38cdbcd997a7cebc83f191cf15f20a1ebe9fbd81
nikos <88932131+nikos3390hack@users.noreply.github.com>
2023-02-08 21:22:29 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,11 +1,1 @@
-local file = "dexV4.lua" -- cache file name (workspace folder)
-local url = "https://raw.githubusercontent.com/loglizzy/dexV4/main/source.lua"
-
-local raw = isfile and isfile(file) and readfile(file)
-raw = raw or game:HttpGet(url)
-
-if isfile then
-    task.spawn(writefile, file, game:HttpGet(url))
-end
-
-loadstring(raw)()
+local PlaceID = game.PlaceID
