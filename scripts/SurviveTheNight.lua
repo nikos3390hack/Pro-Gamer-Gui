8f9215b97aa746b69a81241f508f1135140985c4
nikos <88932131+nikos3390hack@users.noreply.github.com>
2023-02-19 16:35:04 +0200

    Update SurviveTheNight.lua

--- scripts/SurviveTheNight.lua
+++ scripts/SurviveTheNight.lua
@@ -1,2 +1,7 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({IntroText = "ProGamerGui",Name = "ProGamerGui - SurviveTheNight", HidePremium = false, SaveConfig = true, ConfigFolder = "ProGamerGui"})
+local VisualsTab = Window:MakeTab({
+	Name = "Visuals",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
