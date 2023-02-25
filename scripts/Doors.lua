4d14206e091f1cf8056f99aea52c5b78fd6d40b5
nikos <88932131+nikos3390hack@users.noreply.github.com>
2023-02-25 12:48:52 +0200

    Update Doors.lua

--- scripts/Doors.lua
+++ scripts/Doors.lua
@@ -1,16 +1,3 @@
-if game.PlaceId == 6839171747 then
-
-task.defer(function()
-   while wait() do
-       pcall(function()
-           workspace.CurrentRooms["0"].StarterElevator.DoorHitbox:Destroy()
-       end)
-   end
-end)
-
-game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.RemoteListener.Disabled = true
-
-end
 local changeswitch
 local fake = {Disconnect = function()end}
 local index;index = hookmetamethod(game.Changed,"__index",function(self,key)
