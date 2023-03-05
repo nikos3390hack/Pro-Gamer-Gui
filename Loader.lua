e5ed62ded33b6e882ba57580bcc9832a35a94343
nikos <88932131+nikos3390hack@users.noreply.github.com>
2023-03-05 13:34:27 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,10 @@
+local games = {
+    [{6839171747}] = "https://raw.githubusercontent.com/nikos3390hack/Pro-Gamer-Gui/main/scripts/Doors.lua",
+}
 
+for ids, url in next, games do
+    if table.find(ids, game.PlaceId) then
+        loadstring(game:HttpGet(url))()
+        break
+    end
+end
